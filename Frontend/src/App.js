8aec8193d5e49700615354091aaeed04683a5f80
JavierSanhueza <jsanhuesa2023@alu.uct.cl>
2024-10-10 04:12:55 -0300

    hok

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -11,6 +11,7 @@ import AuthPage from './components/AuthPage';
 import GestionEnv from './components/GestionEnv';
 import UserManagementPage from './components/UserManagementPage';
 import GestionVeh from './components/GestionVeh';
+import ReportsPage from './components/ReportsPage';
 
 import './Css/App.css'; // Importa los estilos
 
@@ -40,6 +41,7 @@ const App = () => {
           <Route path="/GestionEnv" element={<GestionEnv />} />
           <Route path="/GestionUser" element={<UserManagementPage />} />
           <Route path="/GestionVeh" element={<GestionVeh />} />
+          <Route path="/ReportsPage" element={<ReportsPage />} />
         </Routes>
       </div>
     </Router>
