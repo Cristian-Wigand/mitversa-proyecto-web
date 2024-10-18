bc53e57e99626ae02f67579d68fb6ac731e8adf8
JavierSanhueza <jsanhuesa2023@alu.uct.cl>
2024-10-18 16:40:46 -0300

    Creacion de grafico de barras y grafico circular de estados de envios

--- Frontend/src/components/ReportsPage.js
+++ Frontend/src/components/ReportsPage.js
@@ -1,7 +1,62 @@
 import React from 'react';
+import { Bar, Pie } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+} from 'chart.js'; // Agrega las importaciones necesarias
 import '../Css/ReportsPage.css';
 
+// Registra los elementos
+ChartJS.register(
+  ArcElement,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+);
+
 const ReportsPage = () => {
+  // Datos de la tabla
+  const envioEstados = [
+    { estado: 'Entregado', cantidad: 100 },
+    { estado: 'No entregado', cantidad: 20 },
+    { estado: 'En tránsito', cantidad: 25 },
+    { estado: 'Devuelto', cantidad: 5 },
+  ];
+
+  // Datos para el gráfico de barras
+  const barData = {
+    labels: envioEstados.map((envio) => envio.estado),
+    datasets: [
+      {
+        label: 'Cantidad de envíos por estado',
+        data: envioEstados.map((envio) => envio.cantidad),
+        backgroundColor: ['#36A2EB', '#FF6384', '#FFCE56', '#4BC0C0'],
+        borderColor: ['#36A2EB', '#FF6384', '#FFCE56', '#4BC0C0'],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  // Datos para el gráfico circular
+  const pieData = {
+    labels: envioEstados.map((envio) => envio.estado),
+    datasets: [
+      {
+        label: 'Porcentaje de entregas',
+        data: envioEstados.map((envio) => envio.cantidad),
+        backgroundColor: ['#36A2EB', '#FF6384', '#FFCE56', '#4BC0C0'],
+        hoverBackgroundColor: ['#36A2EB', '#FF6384', '#FFCE56', '#4BC0C0'],
+      },
+    ],
+  };
+
   return (
     <div className="reports-page">
       <h1>Panel de Informes</h1>
@@ -16,13 +71,19 @@ const ReportsPage = () => {
         </div>
       </div>
 
-      {/* Recuadro de gráficos */}
+      {/* Gráfico de barras de estados de envío */}
       <div className="charts-box">
-        <h2>Gráficos</h2>
-        <div className="charts-content">
-          {/* Aquí puedes insertar tus gráficos */}
-          <p>Gráfico 1: ...</p>
-          <p>Gráfico 2: ...</p>
+        <h2>Estados de Envío</h2>
+        <div className="chart">
+          <Bar data={barData} options={{ responsive: true }} />
+        </div>
+      </div>
+
+      {/* Gráfico de porcentaje de entregas */}
+      <div className="charts-box">
+        <h2>Porcentaje de Entregas por Estado</h2>
+        <div className="chart">
+          <Pie data={pieData} />
         </div>
       </div>
     </div>
